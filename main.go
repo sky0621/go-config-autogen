fd5cde9c9e266099af6cc755a0937ba227102f98
sky0621 <sky14work@gmail.com>
2017-06-29 02:27:58 +0900

    BurntSushi

--- main.go
+++ main.go
@@ -3,15 +3,24 @@ package main
 import (
 	"flag"
 
-	"github.com/spf13/viper"
+	"fmt"
+
+	"github.com/BurntSushi/toml"
 )
 
 // TODO 機能実現スピード最優先での実装なので要リファクタ
 func main() {
 	configFilePath := flag.String("config", "_sample/config.toml", "Config File")
-	template := flag.String("template", "_template/config.go", "Template File")
+	//template := flag.String("template", "_template/config.go", "Template File")
 	flag.Parse()
 
-	viper.SetConfigFile(*configFilePath)
-	viper.ReadInConfig()
+	v := make(map[string]interface{}, 1)
+	metadata, err := toml.Decode(*configFilePath, v)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%#v\n", v)
+	fmt.Printf("%#v\n", metadata)
+	//viper.SetConfigFile(*configFilePath)
+	//viper.ReadInConfig()
 }
