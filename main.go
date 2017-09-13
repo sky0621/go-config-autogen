3b8b2cc5fa15d2789b948e98482f1c37f0b28a62
sky0621 <sky14work@gmail.com>
2017-09-14 00:36:30 +0900

    toml parse

--- main.go
+++ main.go
@@ -1,25 +1,23 @@
 package main
 
 import (
-	"flag"
+	"os"
+
 	"fmt"
 
-	gotoml "github.com/pelletier/go-toml"
+	"github.com/naoina/toml"
 )
 
-// TODO 機能実現スピード最優先での実装なので要リファクタ
 func main() {
-	configFilePath := flag.String("config", "_sample/config.toml", "Config File")
-	//template := flag.String("template", "_template/config.go", "Template File")
-	flag.Parse()
-
-	tree, err := gotoml.LoadFile(*configFilePath)
+	f, err := os.Open("example.toml")
 	if err != nil {
 		panic(err)
 	}
+	defer f.Close()
+	var config map[string]interface{}
+	if err := toml.NewDecoder(f).Decode(&config); err != nil {
+		panic(err)
+	}
 
-	fmt.Printf("[tree]%#v\n\n", tree)
-	fmt.Printf("[tree.ToMap()]%#v\n\n", tree.ToMap())
-	fmt.Printf("[tree.Keys()]%#v\n\n", tree.Keys())
-	fmt.Printf("[tree.String()]%#v\n\n", tree.String())
+	fmt.Println(config)
 }
